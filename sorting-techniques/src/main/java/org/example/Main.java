e0d33ae9a783d876ffbe5e28323479ee27b1a6f8
Ibrahim Mohammed Wajid <ibrahimmohamm@enphaseenergy.com>
2025-09-09 16:33:38 +0530

    added selection sort code

--- sorting-techniques/src/main/java/org/example/Main.java
+++ sorting-techniques/src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = {6, 1, 3, 2, 15, -1, -3, 19, 0};
 
-        bubbleSort(arr);
+        selectionSort(arr);
     }
 
     public static void bubbleSort(int[] arr) {
@@ -20,6 +20,30 @@ public class Main {
 
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + ", ");
+        }
+    }
+
+    public static void selectionSort(int[] arr) {
+        for(int i = 0; i < arr.length - 1; i++) {
+            int min = arr[i];
+            int minIndex = i;
+
+            for(int j = i + 1; j < arr.length; j++) {
+                if(arr[j] < min) {
+                    min = arr[j];
+                    minIndex = j;
+                }
+            }
+
+            if(minIndex != i) {
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+
+            System.out.print("Array is ");
+            for(int k = 0; k < arr.length; k++) System.out.print(arr[k] + " ");
+            System.out.println("");
         }
     }
 }
